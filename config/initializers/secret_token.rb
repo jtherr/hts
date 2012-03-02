509498c28a02937b9b4be8426e28a095d673588d
Jonathan Herr <jtherr@gmail.com>
2012-03-02 09:09:23 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hts::Application.config.secret_token = 'f5a1842bff01d9b27840f7e147df57c44c0dee630c16301566f310fc8ca1533477716cacbae1ffc2467e472bff5b6855333997647a672e5b603eee9058eb9c0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hts::Application.config.session_store :cookie_store, :key => '_hts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hts::Application.config.session_store :active_record_store
