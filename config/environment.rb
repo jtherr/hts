038b522bf9586a168984f140d80ba16d6aa4dd0e
Jonathan Herr <jtherr@gmail.com>
2012-03-17 18:10:31 -0400

    action changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Hts::Application.initialize!
-
-ENV['RECAPTCHA_PUBLIC_KEY'] = '6LeRKwMAAAAAAPbvPVIrpSmxfN093uRQi0h8_gV-'
-ENV['RECAPTCHA_PRIVATE_KEY'] = '6LeRKwMAAAAAAK5RIid5U2Yz5xHJavzcQ2bEwrOe'
