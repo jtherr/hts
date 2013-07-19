3f4674f2aefbc1ac7d40b08d35641bdabf044dfa
Jonathan Herr <jtherr@gmail.com>
2013-07-18 20:51:09 -0400

    fix contacts page

--- app/controllers/quote_requests_controller.rb
+++ app/controllers/quote_requests_controller.rb
@@ -1,6 +1,9 @@
 class QuoteRequestsController < ApplicationController
   before_filter :login_required, :only => [ :index, :desroy ]
 
+  def index
+  end
+
   def new
     @quote_request = QuoteRequest.new
     @title = "Request Quote"
@@ -10,16 +13,12 @@ class QuoteRequestsController < ApplicationController
     @quote_request = QuoteRequest.new(quote_request_params)
     @quote_request.date = DateTime.now
 
-    # if leave_blank is not blank, it is likely a robot, so don't save
-    if params[:leave_blank] == "" && @quote_request.save
+    # if hts is not blank, it is likely a robot, so don't save
+    if params[:hts] == "" && @quote_request.save
       QuoteMailer.quote_request_email(@quote_request).deliver
       @title = "Request Submitted"    
     
-      if logged_in?
-        redirect_to :action => 'index'
-      else
-        redirect_to root_url, :notice => "Thank You - We will contact you shortly"
-      end
+      redirect_to root_url, :notice => "Thank You - We will contact you shortly"
     else
       @title = "Request Quote"
       render :action => 'new'
