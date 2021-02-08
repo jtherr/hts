27d754bedfdb7252ba55d21cedded6a3da5e0074
Jonathan Herr <jonherrm@amazon.com>
2021-02-08 11:49:28 -0500

    Email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,5 +68,5 @@ Hts::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = YAML.load(File.read("../email.yml"))
+  config.action_mailer.smtp_settings = YAML.load(File.read("config/email.yml"))
 end
