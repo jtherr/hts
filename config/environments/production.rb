03955a0d4c456cb967a5730da3ee7a700a12ce60
Jonathan Herr <jonherrm@amazon.com>
2021-02-08 22:49:25 -0500

    fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,5 +68,5 @@ Hts::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = YAML.load(File.read("config/email.yml"))
+  config.action_mailer.smtp_settings = YAML.load(File.read(Rails.root.join('config', 'email.yml')))
 end
